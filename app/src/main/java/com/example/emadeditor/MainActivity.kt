d090e9c0837cc290a691555d2ab7dea529f1e356
emadalqudami17-droid <emadalqudami17-droid@users.noreply.github.com>
2026-09-20 19:57:11 +0300

    Fix: remove aalto-xml to resolve duplicate classes conflict

--- app/src/main/java/com/example/emadeditor/MainActivity.kt
+++ app/src/main/java/com/example/emadeditor/MainActivity.kt
@@ -15,11 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // CRITICAL: Required for Apache POI to work on Android
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl")
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl")
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl")
-
         val layout = LinearLayout(this).apply {
             orientation = LinearLayout.VERTICAL
             setPadding(50, 50, 50, 50)
